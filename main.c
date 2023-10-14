1ab87a16ccc5b44f32739759ac41f8e758a2a9b7
diegopovalz <alejopov@gmail.com>
2023-10-14 13:52:19 -0500

    more changes to cpu profiler

--- main.c
+++ main.c
@@ -1,39 +1,46 @@
 #include <stdio.h>
-#include <unistd.h>  // Include for sleep()
+#include <unistd.h>
+#include <time.h>
 
-#define DUMP(func, call) \
-  printf("%s: func = %p, called by = %p\n", __FUNCTION__, func, call)
+#define DUMP(func, call, time) \
+  printf("%s: func = %p, called by = %p, time = %.2f seconds\n", __FUNCTION__, func, call, (double)time)
+
+clock_t start_time;
 
 void __attribute__((no_instrument_function))
 __cyg_profile_func_enter(void *this_func, void *call_site)
 {
-  DUMP(this_func, call_site);
+    start_time = clock();
+    DUMP(this_func, call_site, 0);  // time is 0 at function entry
 }
 
 void __attribute__((no_instrument_function))
 __cyg_profile_func_exit(void *this_func, void *call_site)
 {
-  DUMP(this_func, call_site);
+    double elapsed_time = ((double)(clock() - start_time)) / CLOCKS_PER_SEC;
+    DUMP(this_func, call_site, elapsed_time);
 }
 
 void sleepy_function(int seconds)
 {
-  printf("Sleeping for %d seconds...\n", seconds);
-  sleep(seconds);
+    printf("\nInside %s()\n", __FUNCTION__);
+    printf("Sleeping for %d seconds...\n", seconds);
+    sleep(seconds);
 }
 
 void another_sleepy_function(int seconds)
 {
-  printf("Sleeping for %d seconds...\n", seconds);
-  sleep(seconds);
+    printf("\nInside %s()\n", __FUNCTION__);
+    printf("Sleeping for %d seconds...\n", seconds);
+    sleep(seconds);
 }
 
 int main(void)
 {
-  printf("In function: %s\n", __FUNCTION__);
+    printf("\nInside %s()\n", __FUNCTION__);
 
-  sleepy_function(2);
-  another_sleepy_function(3);
+    sleepy_function(2);
+    another_sleepy_function(3);
 
-  return 0;
+    return 0;
 }
